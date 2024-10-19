2ae75522a6ba0f94e895d69340963ac9e4c9d590
gunj09 <gunj09waghela@gmail.com>
2024-10-20 01:07:01 +0530

    portfolio

---
+++ app/my-projects/page.tsx
@@ -0,0 +1,34 @@
+"use client"
+import ProjectCard from '@/components/ProjectCard'
+import { Projects } from '@/constants'
+import React from 'react'
+
+const Page = () => {
+    return (
+      <div
+        style={{ backgroundImage: "url(/mountains.jpg)" }}
+        className="w-screen h-screen flex items-center justify-center bg-center bg-cover"
+      >
+        <div className="grid grid-cols-2 gap-5 max-w-[90%] max-h-[90%]">
+          {Projects.map((project, index) => (
+            <a 
+              key={index}
+              href={project.link}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="transition-transform transform hover:scale-105"
+            >
+              <ProjectCard
+                title={project.title}
+                text={project.text}
+                image={project.src}
+              />
+            </a>
+          ))}
+        </div>
+      </div>
+    );
+  };
+  
+  export default Page
+  
---
+++ constants/index.ts
@@ -0,0 +1,145 @@
+import { RxHome, RxPerson, RxDashboard, RxClipboard } from "react-icons/rx";
+
+export const SkillData = [
+  {
+    name: "Html 5",
+    Image: "/html.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Css",
+    Image: "/css.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "JavaScript",
+    Image: "/js.png",
+    width: 65,
+    height: 65,
+  },
+  {
+    name: "Tailwind Css",
+    Image: "/tailwind.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "React",
+    Image: "/react.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Redux",
+    Image: "/redux.png",
+    width: 80,
+    height: 80,
+  },
+
+  {
+    name: "TypeScript",
+    Image: "/ts.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Next js 13",
+    Image: "/next.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Framer Motion",
+    Image: "/framer.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Stripe Payment",
+    Image: "/stripe.webp",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Node js",
+    Image: "/node-js.png",
+    width: 80,
+    height: 80,
+  },
+  {
+    name: "Mongo db",
+    Image: "/mongodb.png",
+    width: 40,
+    height: 40,
+  },
+];
+
+export const Socials = [
+  {
+    name: "Instagram",
+    src: "/instagram.svg",
+    link:"https://www.instagram.com/gunj_0902/?next=%2F",
+  },
+  {
+    name: "Linkedin",
+    src: "/linkedin.svg",
+    link:"https://www.linkedin.com/in/gunj-waghela-6bb858293/",
+  },
+  {
+    name: "Github",
+    src: "/github.svg",
+    link:"https://github.com/gunj09",
+  },
+];
+
+export const Projects = [
+  {
+    title: "Restaurant app UI",
+    text: "Seamless restaurant experience.",
+    src: "/restaurant.png",
+    link: "https://www.figma.com/design/R0e7M7b6PFs4dcZVP5rHmY/Restraunt-menu-ui%2Fux-task?node-id=0-1&t=UrYENKT1ieGlKHH7-1"
+  },
+  {
+    title: "Ecommerce Website",
+    text: ".",
+    src: "/ecommerce.png",
+    link: "https://www.figma.com/design/MRvktWDtfL1MfVNCs0Q5XW/ecommerce?node-id=0-1&t=XoyTkEyQj0Lue859-1"
+  },
+  {
+    title: "Property Rental Website",
+    text: ".",
+    src: "/property.png",
+    link: "https://www.figma.com/design/V4Np0jBTgyaArDF5LlDYHL/Property-rent-round-3?node-id=0-1&t=vxzGwrXeHhpVZvX2-1"
+  },
+  {
+    title: "Education Website",
+    text: ".",
+    src: "/edu.png",
+    link: "https://www.figma.com/design/5BTIQCM7aqsZOkjAEukZwX/Edunote-website?node-id=0-1&t=oh7Fm8G9GTovqBwB-1"
+  }
+];
+
+export const NavLinks = [
+  {
+    name: "/",
+    icon: RxHome,
+    link: "/",
+  },
+  {
+    name: "/my-skills",
+    icon: RxPerson,
+    link: "/my-skills",
+  },
+  {
+    name: "/my-projects",
+    icon: RxDashboard,
+    link: "/my-projects",
+  },
+  {
+    name: "/contact-me",
+    icon: RxClipboard,
+    link: "/contact-me",
+  },
+];
